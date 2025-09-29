43da8fd33852e413eb4c05d33e4581e9e91f8168
Lias1294 <liasyaputri94@gmail.com>
2025-09-29 23:02:19 +0700

    dayone

---
+++ belajar/resources/views/buku/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+@section('title','Tambah Buku')
+    
+@section('content')
+<h2>Tambah Buku </h2>
+   
+
+     <a href="{{route('buku.create')}}" class="btn btn-info"> Tambah Data Buku</a>
+
+    
+@endsection
---
+++ belajar/resources/views/buku/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+@section('title','Buku')
+    
+@section('content')
+<h2>Buku </h2>
+   
+
+     <a href="{{route('buku.create')}}" class="btn btn-primary"></a>
+
+    
+@endsection
